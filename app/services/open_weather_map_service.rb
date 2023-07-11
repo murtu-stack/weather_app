65503311481487cd9f7072bc172e232d3e2fcf50
mdmurtaza <murtaza-cogo@cogoport.com>
2023-07-11 16:52:26 +0530

    first commit

--- app/services/open_weather_map_service.rb
+++ app/services/open_weather_map_service.rb
@@ -1,15 +1,18 @@
 
 class OpenWeatherMapService
-  API_KEY = '7edc36e3aefaab4871bc984653bbd3de'
- 
-  def self.fetch_weather_data
-    response = HTTParty.get(https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=7edc36e3aefaab4871bc984653bbd3de)
-    weather_data = JSON.parse(response.body)
-    # Process and use the weather_data as needed
-
-    temperature = weather_data['main']['temp']
-
-    WeatherData.create(city: city, temperature: temperature)
-
+    API_KEY = '7edc36e3aefaab4871bc984653bbd3de'
+    city='LONDON';
+  
+    def self.fetch_weather_data
+      url = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=/1/1/1.png?appid=7edc36e3aefaab4871bc984653bbd3de"
+      response = HTTParty.get(url)
+      weather_data = JSON.parse(response.body) if response.body.present?
+  
+      if weather_data.present? && weather_data['current'].present?
+        temperature = weather_data['current']['temp']
+        WeatherData.create(city: city, temperature: temperature)
+      else
+        # Handle the situation where weather_data is nil or doesn't have the expected structure
+      end
+    end
   end
-end
